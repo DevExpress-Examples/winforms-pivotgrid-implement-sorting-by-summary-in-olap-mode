9909cfd0c705e1790169688c18e96df149fea2b2
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-10-28 14:20:01 +0400

    Update Form1.cs

--- CS/XtraPivotGrid_OLAPSortBySummary/Form1.cs
+++ CS/XtraPivotGrid_OLAPSortBySummary/Form1.cs
@@ -11,24 +11,19 @@ namespace XtraPivotGrid_OLAPSortBySummary {
             // Expands the Australia column to be able to retrieve OLAP members 
             // that correspond to the nested columns.
             pivotGridControl1.ExpandValue(true, new object[] { "Australia" });
-
             // Obtains OLAP members corresponding to the Australia and Bendigo values.
             IOLAPMember countryMember = pivotGridControl1.GetFieldValueOLAPMember(fieldCountry, 0);
             IOLAPMember cityMember = pivotGridControl1.GetFieldValueOLAPMember(fieldCity, 0);
-            
             // Exits if the OLAP members were not obtained successfully.
             if (countryMember == null || cityMember == null)
                 return;
-
             // Locks the pivot grid from updating while the Sort by Summary
             // settings are being customized.
             pivotGridControl1.BeginUpdate();
             try {
-
                 // Specifies a data field whose summary values should be used to sort values
                 // of the Fiscal Year field.
                 fieldFiscalYear.SortBySummaryInfo.Field = fieldInternetSalesAmount;
-
                 // Specifies a column by which the Fiscal Year field values should be sorted.
                 fieldFiscalYear.SortBySummaryInfo.Conditions.Add(
                     new PivotGridFieldSortCondition(fieldCountry, "Australia", countryMember.UniqueName));
